0905831b761292009fa9af21ed19ddb4e323c74d
Teacher <Teacher@teacher>
2016-09-07 10:23:50 +0800

    js

---
+++ WebContent/javascript/calculator3.js
@@ -0,0 +1,48 @@
+/**
+ * level 3 : new Calculator()  <constructor function>
+ */
+
+function Calculator(){
+	this.sum = 0;
+	this.note = '';
+	
+	this.plus = function(value){
+		this.sum += value;
+		this.note += ' + ' + value;
+	};
+	this.minus = function(value){
+		this.sum -= value;
+		this.note += ' - ' + value;
+	};
+	this.multiple = function(value){
+		this.sum *= value;
+		this.note += ' * ' + value;
+	};
+	this.divide = function(value){
+		this.sum /= value;
+		this.note += ' / ' + value;
+	};
+	this.init =function(value){
+		if(arguments.length == 0){
+			this.sum = 0;
+			this.note = '';
+		}else{
+			this.sum = value;
+			this.note += + value;
+		}
+	};
+	
+	this.printResult = function(){
+		console.log(this.note + " = " + this.sum);
+	};
+}
+
+
+
+
+
+
+
+
+
+
